58eba08b0f7e4000178ab88c91ca3c9a0f71fa79
Vadim Kononov <vadimkononov@tactica.ca>
2023-11-23 13:44:05 -0600

    Add a default CLI command and remove "report" from the CLI.

--- test/package/audit/technology/test_errors.rb
+++ test/package/audit/technology/test_errors.rb
@@ -6,19 +6,19 @@ module Package
   module Audit
     class TestErrors < Minitest::Test
       def test_that_it_prints_an_error_about_a_file_instead_of_a_directory
-        output = `bundle exec package-audit report test/files/gemfile/report/Gemfile`
+        output = `bundle exec package-audit test/files/gemfile/report/Gemfile`
 
         assert_match '"test/files/gemfile/report/Gemfile" is a file instead of directory', output
       end
 
       def test_that_it_prints_an_error_about_a_non_existing_directory
-        output = `bundle exec package-audit report test/files/gemfile/report/invalid`
+        output = `bundle exec package-audit test/files/gemfile/report/invalid`
 
         assert_match '"test/files/gemfile/report/invalid" is not a valid directory', output
       end
 
       def test_that_it_prints_an_error_when_no_supported_technologies_are_found
-        output = `bundle exec package-audit report test/files/`
+        output = `bundle exec package-audit test/files/`
 
         assert_match 'No supported technologies found in this directory', output
       end
--- test/package/audit/test_group.rb
+++ test/package/audit/test_group.rb
@@ -7,42 +7,42 @@ module Package
   module Audit
     class TestGroup < Minitest::Test
       def test_that_no_group_produces_expected_output
-        output = `bundle exec package-audit report test/files/gemfile/groups`
+        output = `bundle exec package-audit test/files/gemfile/groups`
 
         %w[irb minitest rack reline tzinfo yard].each { |pkg| assert_match pkg, output }
         assert_match 'Found a total of 6 ruby packages.', output
       end
 
       def test_that_default_group_produces_expected_output
-        output = `bundle exec package-audit report test/files/gemfile/groups -g default`
+        output = `bundle exec package-audit test/files/gemfile/groups -g default`
 
         %w[irb rack reline].each { |pkg| assert_match pkg, output }
         assert_match 'Found a total of 3 ruby packages.', output
       end
 
       def test_that_development_group_produces_expected_output
-        output = `bundle exec package-audit report test/files/gemfile/groups -g development`
+        output = `bundle exec package-audit test/files/gemfile/groups -g development`
 
         %w[irb rack reline yard].each { |pkg| assert_match pkg, output }
         assert_match 'Found a total of 4 ruby packages.', output
       end
 
       def test_that_test_group_produces_expected_output
-        output = `bundle exec package-audit report test/files/gemfile/groups -g test`
+        output = `bundle exec package-audit test/files/gemfile/groups -g test`
 
         %w[irb minitest rack reline].each { |pkg| assert_match pkg, output }
         assert_match 'Found a total of 4 ruby packages.', output
       end
 
       def test_that_production_group_produces_expected_output
-        output = `bundle exec package-audit report test/files/gemfile/groups -g production`
+        output = `bundle exec package-audit test/files/gemfile/groups -g production`
 
         %w[irb rack reline tzinfo].each { |pkg| assert_match pkg, output }
         assert_match 'Found a total of 4 ruby packages.', output
       end
 
       def test_that_multiple_groups_produces_expected_output
-        output = `bundle exec package-audit report test/files/gemfile/groups -g test -g production`
+        output = `bundle exec package-audit test/files/gemfile/groups -g test -g production`
 
         %w[irb minitest rack reline tzinfo].each { |pkg| assert_match pkg, output }
         assert_match 'Found a total of 5 ruby packages.', output
