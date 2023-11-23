295d54e9726c2117941219102e740ce907c7eef6
Vadim Kononov <vadimkononov@tactica.ca>
2023-11-23 14:58:57 -0600

    Fix an issue with handling directory paths with spaces.

--- lib/package/audit/const/cmd.rb
+++ lib/package/audit/const/cmd.rb
@@ -3,13 +3,13 @@ module Package
     module Const
       module Cmd
         BUNDLE_AUDIT = 'bundle-audit check --update'
-        BUNDLE_AUDIT_JSON = 'bundle-audit check --update --quiet --format json %s 2>/dev/null'
+        BUNDLE_AUDIT_JSON = 'bundle-audit check --update --quiet --format json "%s" 2>/dev/null'
 
         NPM_AUDIT = 'npm audit'
         NPM_AUDIT_JSON = 'npm audit --json'
 
         YARN_AUDIT = 'yarn audit'
-        YARN_AUDIT_JSON = 'yarn audit --json --cwd %s'
+        YARN_AUDIT_JSON = 'yarn audit --json --cwd "%s"'
       end
     end
   end
