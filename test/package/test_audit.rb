1e409a24f8626d99019564577dfb382e2bc636bd
Vadim Kononov <vadimkononov@tactica.ca>
2024-03-05 14:36:09 -0600

    Display detected technologies when running the tool.

--- test/package/test_audit.rb
+++ test/package/test_audit.rb
@@ -60,7 +60,7 @@ module Package
       output = `bundle exec package-audit test/files/gemfile/report`
 
       assert_match 'Found a total of 3 ruby packages.', output
-      assert_match '1 vulnerable (8 vulnerabilities), 2 outdated, 1 deprecated.', output
+      assert_match '1 vulnerable (11 vulnerabilities), 2 outdated, 1 deprecated.', output
     end
 
     def test_that_there_is_a_message_about_outdated_gems
